8ad118ed8b75546deba4cc88ddede815a7b3d8bd
ValeryFrenkel <valeron997@gmail.com>
2025-09-05 19:26:14 +0300

    Run all tests

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -1,36 +1,9 @@
-from json import loads
-
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from api_mailhog.apis.mailhog_api import MailhogApi
-from helpers.account_helper import AccountHelper
-from restclient.configuration import Configuration as MailhogConfiguration
-from restclient.configuration import Configuration as DmApiConfiguration
-from services.dm_api_account import DMApiAccount
-from services.api_mailhog import MailHogApi
-import structlog
-
-structlog.configure(
-    processors=[
-        structlog.processors.JSONRenderer(
-            indent=4,
-            ensure_ascii=True,
-            # sort_keys=True
-        )
-    ]
-)
-
-
-def test_post_v1_account_login():
-    # Регистрация пользователя
-    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
-    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
-
-    account = DMApiAccount(configuration=dm_api_configuration)
-    mailhog = MailHogApi(configuration=mailhog_configuration)
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
-    login = 'vfrenkel_test69'
-    password = '123456789'
-    email = f'{login}@example.com'
+def test_post_v1_account_login(
+        account_helper,
+        prepare_user
+):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
     account_helper.register_new_user(login=login, password=password, email=email)
     account_helper.user_login(login=login, password=password)
